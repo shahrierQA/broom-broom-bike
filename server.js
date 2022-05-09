c1efc39b416b5258af4f95d901619f1f85feaa89
shahrierQA <shahrierfahim34@gmail.com>
2022-05-09 17:25:42 +0600

    Add SIGTERM signal on server

--- server.js
+++ server.js
@@ -33,3 +33,11 @@ process.on("unhandledRejection", (err) => {
     process.exit(1);
   });
 });
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM RECEIVED! Shutting down......");
+
+  server.close(() => {
+    console.log("Process terminated.......");
+  });
+});
